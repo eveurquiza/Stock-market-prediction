a03488c6f416ebdcc9227c15b7c0335a62983abd
Everardo Urquiza <eveurquiza2@gmail.com>
2026-06-22 23:41:09 -0600

    Feature Engineering in features file

---
+++ src/features/build_features.py
@@ -0,0 +1,26 @@
+from ta.trend import SMAIndicator
+from ta.momentum import RSIIndicator
+
+df['SMA_20'] = SMAIndicator(
+    close=df['Close'],
+    window=20
+).sma_indicator()
+df['SMA_50'] = SMAIndicator(
+    close=df['Close'],
+    window=50
+).sma_indicator()
+
+df['EMA_20'] = SMAIndicator(
+    close=df['Close'],
+    window=20,
+    fillna=True
+).sma_indicator()
+
+df['RSI'] = RSIIndicator(
+    close=df['Close']
+).rsi()
+
+df['MACD'] = df['Close'].ewm(span=12, adjust=False).mean() - df['Close'].ewm(span=26, adjust=False).mean()
+
+df['Bollinger_Band'] = df['Close'].rolling(window=20).mean() + (df['Close'].rolling(window=20).std() * 2)
+
