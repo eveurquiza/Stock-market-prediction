db54d26cd616314d13a5377ddecfa3d3c8d2c845
Everardo Urquiza <eveurquiza2@gmail.com>
2026-06-23 23:36:21 -0600

    Update download_data.py for many stocks

--- src/data/download_data.py
+++ src/data/download_data.py
@@ -1,6 +1,8 @@
 import yfinance as yf
 
-ticker = "AAPL"
+options = {'01': 'AAPL', '02': '^GSPC', '03': 'GOOGL', '04': 'AMZN', '05': 'TSLA'}
+print(f"Available options: {list(options.values())}")
+ticker = input("Enter the ticker symbol you want to download (e.g., AAPL, GSPC, GOOGL, AMZN, TSLA): ").upper()
 
 df = yf.download(
     ticker,
@@ -12,6 +14,6 @@ df = yf.download(
 df.reset_index(inplace=True)
 
 df.to_csv(
-    "data/raw/aapl.csv",
+    f"data/raw/{ticker.lower()}.csv",
     index=False
 )
